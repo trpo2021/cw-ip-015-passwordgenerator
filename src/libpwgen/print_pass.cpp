0b74fa4e8c83efaafd8e2ca04e71498ee63a2f39
proof235 <rusik2garin@mail.ru>
2021-05-30 15:18:57 +0700

    Format code

--- src/libpwgen/print_pass.cpp
+++ src/libpwgen/print_pass.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-void print_pass(char password[],int quantity_pass_symbols) {
-	cout << "Your password: ";
-	for (int i = 0; i < quantity_pass_symbols; i++) {
-		cout << password[i];
-	}
-	cout << endl << endl;
+void print_pass(char password[], int quantity_pass_symbols)
+{
+    cout << "Your password: ";
+    for (int i = 0; i < quantity_pass_symbols; i++) {
+        cout << password[i];
+    }
+    cout << endl << endl;
 }
--- src/libpwgen/rand_capital_letter.cpp
+++ src/libpwgen/rand_capital_letter.cpp
@@ -1,10 +1,11 @@
 #include "rand_capital_letter.h"
-#include <iostream>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
+#include <iostream>
 
 using namespace std;
-char rand_capital_letter() {
-	char rand_symbol = rand() % 25 + 65; 
-	return rand_symbol;
+char rand_capital_letter()
+{
+    char rand_symbol = rand() % 25 + 65;
+    return rand_symbol;
 }
