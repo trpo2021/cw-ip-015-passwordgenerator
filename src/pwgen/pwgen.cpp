e85c9815cf867f5678b975ffc760a4c248ac6942
proof235 <rusik2garin@mail.ru>
2021-05-30 20:28:54 +0700

    Add pass capital letter exclude

--- src/pwgen/pwgen.cpp
+++ src/pwgen/pwgen.cpp
@@ -5,6 +5,7 @@
 #include <libpwgen/rand_capital_letter.h>
 #include <libpwgen/rand_cursive_letter.h>
 #include <libpwgen/rand_numeral.h>
+#include <libpwgen/rand_capital_letter_exclude.h>
 
 using namespace std;
 
@@ -25,5 +26,10 @@ int main()
         password[i] = rand_cursive_letter();
     }
     print_pass(password, quantity_pass_symbols);
-    return 0;
+    for (int i = 0; i < quantity_pass_symbols; i++) {
+        password[i] = rand_capital_letter_exclude();
+    }
+    print_pass(password, quantity_pass_symbols);
+    
+return 0;
 }
