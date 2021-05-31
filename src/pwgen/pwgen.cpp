d6a3bde2bc59a9c17a87b7af5dac9479784f7134
koynovbro <konstantin.koynov@gmail.com>
2021-06-01 02:50:34 +0700

    Add a call to the 'menu' function

--- src/pwgen/pwgen.cpp
+++ src/pwgen/pwgen.cpp
@@ -10,6 +10,11 @@
 #include <libpwgen/rand_numeral.h>
 #include <libpwgen/rand_special_symbols.h>
 #include <libpwgen/requests_for_user.h>
+#include <libpwgen/menu.h>
+#include <libpwgen/query_question_what_pass.h>
+
+
+
 
 using namespace std;
 
@@ -18,7 +23,7 @@ int choice, quantity_pass, quantity_pass_symbols;
 int main()
 {
     srand(time(0));
-    pass_choice_symbols();
+    menu();
 
     return 0;
 }
