30f89ca995f549dcf4fb2a02d9e2d6230657018c
koynovbro <konstantin.koynov@gmail.com>
2021-06-01 03:09:39 +0700

    Format code

--- src/libpwgen/query_question_what_pass.cpp
+++ src/libpwgen/query_question_what_pass.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
 #include "query_question_what_pass.h"
+#include <iostream>
 
 using namespace std;
 
-int query_question_what_pass() {
-	int choice;
-	cout << "Select the option you want:" << endl
-		<< "1. Password with a choice of symbols" << endl
-		<< "2. Password with all symbols" << endl
-		<< "3. Password with random characters." << endl
-		<< "Enter the item number: ";
-	cin >> choice;
-	return choice;
+int query_question_what_pass()
+{
+    int choice;
+    cout << "Select the option you want:" << endl
+         << "1. Password with a choice of symbols" << endl
+         << "2. Password with all symbols" << endl
+         << "3. Password with random characters." << endl
+         << "Enter the item number: ";
+    cin >> choice;
+    return choice;
 }
--- src/pwgen/pwgen.cpp
+++ src/pwgen/pwgen.cpp
@@ -1,8 +1,10 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <libpwgen/menu.h>
 #include <libpwgen/pass_choice_symbols.h>
 #include <libpwgen/print_pass.h>
+#include <libpwgen/query_question_what_pass.h>
 #include <libpwgen/rand_capital_letter.h>
 #include <libpwgen/rand_capital_letter_exclude.h>
 #include <libpwgen/rand_cursive_letter.h>
@@ -10,11 +12,6 @@
 #include <libpwgen/rand_numeral.h>
 #include <libpwgen/rand_special_symbols.h>
 #include <libpwgen/requests_for_user.h>
-#include <libpwgen/menu.h>
-#include <libpwgen/query_question_what_pass.h>
-
-
-
 
 using namespace std;
 
