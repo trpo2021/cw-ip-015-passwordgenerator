60205dd55407762b305988443144a2ff1b0fee94
koynovbro <konstantin.koynov@gmail.com>
2021-06-01 02:46:06 +0700

    Create function for request password type 'query_question_what_pass'

---
+++ src/libpwgen/query_question_what_pass.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "query_question_what_pass.h"
+
+using namespace std;
+
+int query_question_what_pass() {
+	int choice;
+	cout << "Select the option you want:" << endl
+		<< "1. Password with a choice of symbols" << endl
+		<< "2. Password with all symbols" << endl
+		<< "3. Password with random characters." << endl
+		<< "Enter the item number: ";
+	cin >> choice;
+	return choice;
+}
