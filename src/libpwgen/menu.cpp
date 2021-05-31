4b193af4706c65e9669a7854fd0733be9137ec78
koynovbro <konstantin.koynov@gmail.com>
2021-06-01 02:48:18 +0700

    Create function user menu 'menu'

---
+++ src/libpwgen/menu.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "libpwgen/query_question_what_pass.h"
+#include "libpwgen/pass_choice_symbols.h"
+#include "libpwgen/pass_all_symbols.h"
+#include "libpwgen/random_character_password.h"
+#include "libpwgen/menu.h"
+
+using namespace std;
+
+void menu() {
+	int choice = 0;
+	while (choice == 0) {
+		choice = query_question_what_pass();
+		switch (choice) {
+		case(1):
+			pass_choice_symbols();
+			break;
+		case(2):
+			pass_all_symbols();
+			break;
+		case(3):
+			pass_randoms_symbols();
+			break;
+		default:
+			choice = 0;
+			system("clear");
+			cout << "Invalid value entered, please try again!" << endl;
+			break;
+		}
+	}
+}
