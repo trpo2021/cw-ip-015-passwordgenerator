35efc9d5e4df3cb7353025840a28bea36b97c352
BarPus <katerinalove02@mail.ru>
2021-06-01 14:08:33 +0700

    Create header file for functions for parameters

---
+++ src/libpwgen/for_parameters.h
@@ -0,0 +1,4 @@
+#pragma once 
+void pass_choice_symbols_for_parameters(int quantity_pass_symbols, int quantity_rand_capital_letter, int quantity_rand_cursive_letter, int quantity_rand_numeral, int quantity_rand_special_symbols, bool choice);
+void fill_rand_symbols_for_parameters(char password[], int marker, int quantity_pass_symbols, bool choice);
+void pass_randoms_symbols_for_parameters(int quantity_pass_symbols, int quantity_pass, bool choice);
